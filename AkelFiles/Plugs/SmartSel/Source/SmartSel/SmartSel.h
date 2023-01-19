bbbf2ed414f2fbae1b7e3f7abbea705e0d2e4a00
ro.edi <122004809+nvdavsamd@users.noreply.github.com>
2023-01-19 09:19:25 +0100

    Add files via upload

---
+++ AkelFiles/Plugs/SmartSel/Source/SmartSel/SmartSel.h
@@ -0,0 +1,15 @@
+#ifndef _smart_sel_h_
+#define _smart_sel_h_
+//---------------------------------------------------------------------------
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <richedit.h>
+#include "StrFunc.h"
+#include "WideFunc.h"
+#include "AkelEdit.h"
+#include "AkelDLL.h"
+#include "Resources\Resource.h"
+
+//---------------------------------------------------------------------------
+#endif
